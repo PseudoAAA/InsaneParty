9474f6fbfa54c01ec207c33cd0b3e21650ea8c3a
PseudoAAA <Dgaudinev@yandex.ru>
2024-03-19 15:33:17 +0300

    0.0.0.5.1

--- Source/InsaneParty/InsanePartyGameMode.cpp
+++ Source/InsaneParty/InsanePartyGameMode.cpp
@@ -5,7 +5,9 @@
 AInsanePartyGameMode::AInsanePartyGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Characters/DefaultCharacter/BP_ThirdPersonCharacter"));
+	
+	
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
