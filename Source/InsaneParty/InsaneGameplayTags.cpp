6907ae63be1bdda5987ec132aa2d9281969f370a
PseudoAAA <Dgaudinev@yandex.ru>
2024-04-07 13:13:32 +0300

    0.0.0.8.6.14

--- Source/InsaneParty/InsaneGameplayTags.cpp
+++ Source/InsaneParty/InsaneGameplayTags.cpp
@@ -18,17 +18,18 @@ namespace InsaneGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Activation_Fail_MissingTags, "Activation.Fail.MissingTags", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Activation_Fail_Networking, "Activation.Fail.Networking", "")
 
-
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Inventory_DropWeapon, "Inventory.DropWeapon", "")
+	
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Inventory_Slot1, "Inventory.Slot1", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Inventory_Slot2, "Inventory.Slot2", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Inventory_Slot3, "Inventory.Slot3", "")
-
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Inventory_DropWeapon, "Inventory.DropWeapon", "")
+	
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Weapon_SwitchFireMode, "Weapon.SwitchFireMode", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Weapon_FireMode_None, "Weapon.FireMode.None", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Weapon_FireMode_Single, "Weapon.FireMode.Single", "")
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Weapon_FireMode_FullAuto, "Weapon.FireMode.FullAuto", "")
 
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(PlayerActions_Shoot, "PlayerActions.Shoot", "")
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(PlayerActions_Aiming, "PlayerActions.Aiming", "")
 }
 
